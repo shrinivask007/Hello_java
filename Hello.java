61353dbdce1ba089cab97bbec7d0a6f632978b94
Shrinivas Kathare <shrinivassk18@gmail.com>
2024-07-28 15:42:01 +0530

    test

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 
-class HelloWorld {
+class Hello {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
